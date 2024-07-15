5032ed613f1486d1c54edda1d801cd2c01ffd6af
AnnaZuzkova <famzu2018@gmail.com>
2024-07-15 17:06:56 +0300

    hw 14

--- index.js
+++ index.js
@@ -124,4 +124,4 @@ const users = [
 // console.log(getUsersWithAge(users, 20, 30)); // [об'єкт Ross Vazquez, об'єкт Elma Head, об'єкт Carey Barr]
 
 // console.log(getUsersWithAge(users, 30, 40));
-// [об'єкт Moore Hensley, об'єкт Sharlene Bush, об'єкт Blackburn Dotson, об'єкт Sheree Anthony]
+// [об'єкт Moore Hensley, об'єкт Sharlene Bush, об'єкт Blackburn Dotson, об'єкт Sheree Anthony]/
